baa64c9a9ba4f140b328eb9fa204468fae2b75d4
Homero Oliveira <homero.santos.001@acad.pucrs.br>
2018-10-25 17:59:43 -0300

    added and created models

---
+++ TMDbApp/Model/Movie.swift
@@ -0,0 +1,16 @@
+//
+//  Movie.swift
+//  TMDbApp
+//
+//  Created by Homero Oliveira on 25/10/18.
+//  Copyright © 2018 Homero Oliveira. All rights reserved.
+//
+
+import Foundation
+
+struct Movie: Decodable {
+    let id: Int
+    let title: String
+    let posterPath: String
+    let releaseDate: Date
+}
